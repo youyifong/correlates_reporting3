286bcc032650e57eece6e2db06b5e203891d7ef4
Youyi Fong <youyifong@gmail.com>
2023-07-29 07:29:48 -0700

    updates

--- cor_coxph/code/cor_coxph_moderna_boost.R
+++ cor_coxph/code/cor_coxph_moderna_boost.R
@@ -89,13 +89,13 @@ for (iObj in 1:2) {
   names(all.markers)=all.markers
 
   # loop through (1) naive, (2) nnaive, (3) pooled
-  for (idat in 1:3) {
-    # idat=3
+  for (iNaive in 1:3) {
+    # iNaive=3
     
-    myprint(idat)
-    if (idat==1) {dat.ph1 = dat.naive;  save.results.to = glue("{save.results.to.0}/obj{iObj}_naive/")}
-    if (idat==2) {dat.ph1 = dat.nnaive; save.results.to = glue("{save.results.to.0}/obj{iObj}_nnaive/")}
-    if (idat==3) {dat.ph1 = dat; save.results.to = glue("{save.results.to.0}/obj{iObj}_pooled/")}
+    myprint(iNaive)
+    if (iNaive==1) {dat.ph1 = dat.naive;  save.results.to = glue("{save.results.to.0}/obj{iObj}_naive/")}
+    if (iNaive==2) {dat.ph1 = dat.nnaive; save.results.to = glue("{save.results.to.0}/obj{iObj}_nnaive/")}
+    if (iNaive==3) {dat.ph1 = dat; save.results.to = glue("{save.results.to.0}/obj{iObj}_pooled/")}
     
     # dat.ph1 = subset(dat.ph1, Ptid!="US3302292")
     # dat.ph1 = subset(dat.ph1, !Ptid %in% c("US3302292", "US3302397", "US3492199", "US3632155"))
@@ -147,7 +147,7 @@ for (iObj in 1:2) {
     
     # marginalized risk and controlled VE
     comp.risk=F
-    COR=idat # only used in table figure labels
+    COR=iNaive # only used in table figure labels
     
     source(here::here("code", "cor_coxph_marginalized_risk_bootstrap.R"))
     
@@ -163,7 +163,34 @@ for (iObj in 1:2) {
 ###################################################################################################
 # Obj 3: To assess whether the CoR in 1. or 2. is modified by SARS-CoV-2 naive/non-naive status
 
+dat.ph1 = dat
+
+# create data objects
+design.1<-twophase(id=list(~1,~1), strata=list(NULL,~Wstratum), subset=~ph2, data=dat.ph1)
+with(dat.ph1, table(Wstratum, ph2, useNA="ifany"))
+
+# prepare for Cox models runs and margialized risks
+tpeak=29
+# the origin of followup days, may be different from tpeak, e.g., D43start48
+tpeak1 = 29
+
+
+# first, assess whether any demo variables are modified by naive
+
+f= update(form.0, ~.+naive*HighRiskInd)
+summary(svycoxph(f, design=design.1))
+
+f= update(form.0, ~.+naive*MinorityInd)
+summary(svycoxph(f, design=design.1))
+
+f= update(form.0, ~.+naive*risk_score)
+summary(svycoxph(f, design=design.1))
+
+# repeat twice, with and without adjusting for naive*risk_score
+for (ind in 1:2) {
+  
 for (iObj in 1:2) {
+# iObj=1
   
   if (iObj==1) {
     all.markers = paste0("BD29", obj.assays)
@@ -172,10 +199,6 @@ for (iObj in 1:2) {
   }
   names(all.markers)=all.markers
 
-  dat.ph1 = dat
-
-  ilabel="all"
-  
   save.results.to = glue("{save.results.to.0}/obj3_{iObj}/")
   if (!dir.exists(save.results.to))  dir.create(save.results.to)
   print(paste0("save results to ", save.results.to))
@@ -183,18 +206,13 @@ for (iObj in 1:2) {
   # save info
   write(tfinal.tpeak, file=paste0(save.results.to, "timepoints_cum_risk"))
   
-  # create data objects
-  design.1<-twophase(id=list(~1,~1), strata=list(NULL,~Wstratum), subset=~ph2, data=dat.ph1)
-  with(dat.ph1, table(Wstratum, ph2, useNA="ifany"))
-  
-  # prepare for Cox models runs and margialized risks
-  tpeak=29
-  # the origin of followup days, may be different from tpeak, e.g., D43start48
-  tpeak1 = 29
-  
   # fit the interaction model and save regression results to a table
   for (a in all.markers) {
-    f= update(form.0, as.formula(paste0("~.+naive*", a)))
+    if (ind==1) {
+      f= update(form.0, as.formula(paste0("~.+naive*", a)))
+    } else{
+      f= update(form.0, as.formula(paste0("~.+naive*", a, "+naive:HighRiskInd")))
+    }
     fits=list(svycoxph(f, design=design.1))
     est=getFormattedSummary(fits, exp=T, robust=T, type=1)
     ci= getFormattedSummary(fits, exp=T, robust=T, type=13)
@@ -209,7 +227,7 @@ for (iObj in 1:2) {
     colnames(tab)=c("HR", "P value")
     # tab=rbind(tab, "Generalized Wald Test for Markers"=c("", formatDouble(p.gwald,3, remove.leading0 = F)))
     tab
-    mytex(tab, file.name=paste0("CoR_itxnnaive_",a), align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
+    mytex(tab, file.name=paste0("CoR_itxnnaive_",a,"_",ind), align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
     # itxn.pvals=c(itxn.pvals, last(getFixedEf(fit)[,"p.value"]))
   }
 
@@ -221,20 +239,19 @@ for (iObj in 1:2) {
   # colnames(tab)=c("interaction P value", "FWER", "FDR")
   # mytex(tab, file.name="CoR_itxn_multitesting", align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
 }
+  
+}
 
 
 ###################################################################################################
 # Obj 4: To assess whether the CoR in 1. is modified by the BD1 antibody value
 
-for (idat in 1:2) {
-  # idat=1
+for (iNaive in 1:2) {
+  # iNaive=1
   
-  all.markers = paste0("BD29", obj.assays)
-  names(all.markers)=all.markers
-  
-  myprint(idat)
-  if (idat==1) {dat.ph1 = dat.naive;  save.results.to = glue("{save.results.to.0}/obj4_naive/")}
-  if (idat==2) {dat.ph1 = dat.nnaive; save.results.to = glue("{save.results.to.0}/obj4_nnaive/")}
+  myprint(iNaive)
+  if (iNaive==1) {dat.ph1 = dat.naive;  save.results.to = glue("{save.results.to.0}/obj4_naive/")}
+  if (iNaive==2) {dat.ph1 = dat.nnaive; save.results.to = glue("{save.results.to.0}/obj4_nnaive/")}
   
   if (!dir.exists(save.results.to))  dir.create(save.results.to)
   print(paste0("save results to ", save.results.to))
@@ -252,8 +269,9 @@ for (idat in 1:2) {
   tpeak1 = 29
   
   # fit the interaction model and save regression results to a table
-  for (a in all.markers) {
-    f= update(form.0, as.formula(paste0("~.+scale(",sub("BD29","BD1",a),")*scale(", a, ")")))
+  for (a in obj.assays) {
+  for (ind in 1:2) { # once for BD29 and one for Delta
+    f= update(form.0, as.formula(paste0("~.+scale(BD1", a ,") * scale(", ifelse(ind==1, "BD29", "DeltaBD29overBD1"), a, ")")))
     fits=list(svycoxph(f, design=design.1))
     est=getFormattedSummary(fits, exp=T, robust=T, type=1)
     ci= getFormattedSummary(fits, exp=T, robust=T, type=13)
@@ -268,8 +286,9 @@ for (idat in 1:2) {
     colnames(tab)=c("HR", "P value")
     # tab=rbind(tab, "Generalized Wald Test for Markers"=c("", formatDouble(p.gwald,3, remove.leading0 = F)))
     tab
-    mytex(tab, file.name=paste0("CoR_itxnBD1BD29_",a), align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
+    mytex(tab, file.name=paste0("CoR_itxn_BD1_",ifelse(ind==1, "BD29", "DeltaBD29overBD1"),"_",a), align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
     # itxn.pvals=c(itxn.pvals, last(getFixedEf(fit)[,"p.value"]))
+  }
   }
   
   # names(itxn.pvals)=config$interaction
@@ -302,18 +321,27 @@ if (!is.null(config$multivariate_assays)) {
   design.1<-twophase(id=list(~1,~1), strata=list(NULL,~Wstratum), subset=~ph2, data=dat.ph1)
   with(dat.ph1, table(Wstratum, ph2, useNA="ifany"))
   
+  for (ind in 1:2) { # repeat twice, with and without naive::HighRiskInd
   for (a in config$multivariate_assays) {
-    for (i in 1:2) {
-      # 1: per SD; 2: per 10-fold
+  for (i in 1:2) { # 1: per SD; 2: per 10-fold
+      
       a.tmp=a
-      aa=trim(strsplit(a, "\\+")[[1]])
-      for (x in aa[!contain(aa, "\\*")]) {
-        # replace every variable with scale(x) when i==1
-        a.tmp=gsub(x, paste0(if(i==1) "scale","(",x,")"), a.tmp) 
+      if (i==1) {
+        # it is key not to trim aa and it is key that a is, e.g. x1 + x2 and not x1+x2, otherwise it won't work
+        aa=strsplit(a, "\\+")[[1]]
+        for (x in aa[!contain(aa, "\\*")]) {
+          # replace every variable with scale(x) when i==1
+          a.tmp=gsub(x, paste0(if(i==1) "scale","(",x,")"), a.tmp) 
+        }
       }
-      f= update(form.0, as.formula(paste0("~.+naive+", a.tmp)))
+      
+      if (ind==1) {
+        f= update(form.0, as.formula(paste0("~.+naive+", a.tmp)))
+      } else{
+        f= update(form.0, as.formula(paste0("~.+naive*HighRiskInd+", a.tmp)))
+      }
       fit=svycoxph(f, design=design.1) 
-      var.ind=length(coef(fit)) - length(aa):1 + 1
+      var.ind = which(sapply(names(coef(fit)), function(x) any(sapply(aa, function (a) contain(x,kyotil::trim(a))))))
       
       # svycoxph(Surv(EventTimePrimary, EventIndPrimary) ~ MinorityInd + HighRiskInd + 
       #            risk_score + scale(BD1pseudoneutid50_BA.1) * scale(BD29pseudoneutid50_BA.1), design=design.1)
@@ -336,13 +364,12 @@ if (!is.null(config$multivariate_assays)) {
       tab
       tab=rbind(tab, "Generalized Wald Test"=c("", formatDouble(p.gwald,3, remove.leading0 = F)))
       
-      mytex(tab, file.name=paste0("CoR_multivariable_svycoxph_pretty", match(a, config$multivariate_assays), if(i==2) "_per10fold", study_name), align="c", include.colnames = T, save2input.only=T, 
+      mytex(tab, file.name=paste0("CoR_multivariable_svycoxph_pretty", match(a, config$multivariate_assays), if(i==2) "_per10fold", "_", ind), align="c", include.colnames = T, save2input.only=T, 
             input.foldername=save.results.to.0)
-    }
   }
-  
+  }
+  }
 }
-
 
 
 
