50df4bb8e790244d29d83a282817644a8a537f2f
Youyi Fong <youyifong@gmail.com>
2023-07-30 14:37:51 -0700

    rename some code files, remove some code files

--- cor_coxph/code/cor_coxph_moderna_boost.R
+++ cor_coxph/code/cor_coxph_moderna_boost.R
@@ -74,6 +74,8 @@ myprint(prev.naive, prev.nnaive, prev.pooled)
 
 
 ###################################################################################################
+cat("Univariate analyses")
+
 # Obj 1: To assess BD29 omicron Ab as a correlate of risk (CoR) against omicron COVID-19 
 # Obj 2: To assess fold-rise in omicron Ab from BD1/pre-booster to BD29 as a CoR against omicron COVID-19
 
@@ -132,8 +134,7 @@ for (iObj in 1:2) {
     # the origin of followup days, may be different from tpeak, e.g., D43start48
     tpeak1 = 29
     
-    source(here::here("code", "cor_coxph_marginalized_risk_no_marker.R"))
-    
+    # Cox regression
     source(here::here("code", "cor_coxph_ph.R"))
     
     
@@ -146,8 +147,11 @@ for (iObj in 1:2) {
     } 
     
     # marginalized risk and controlled VE
+    
     comp.risk=F
     COR=iNaive # only used in table figure labels
+    
+    source(here::here("code", "cor_coxph_marginalized_risk_no_marker.R"))
     
     source(here::here("code", "cor_coxph_marginalized_risk_bootstrap.R"))
     
@@ -161,7 +165,7 @@ for (iObj in 1:2) {
 
 
 ###################################################################################################
-# Obj 3: To assess whether the CoR in 1. or 2. is modified by SARS-CoV-2 naive/non-naive status
+cat("Obj 3: To assess whether the CoR in 1. or 2. is modified by SARS-CoV-2 naive/non-naive status by fitting interaction models")
 
 dat.ph1 = dat
 
@@ -244,7 +248,7 @@ for (iObj in 1:2) {
 
 
 ###################################################################################################
-# Obj 4: To assess whether the CoR in 1. is modified by the BD1 antibody value
+cat("Obj 4: To assess whether the CoR in 1. is modified by the BD1 antibody value")
 
 for (iNaive in 1:2) {
   # iNaive=1
@@ -253,6 +257,8 @@ for (iNaive in 1:2) {
   if (iNaive==1) {dat.ph1 = dat.naive;  save.results.to = glue("{save.results.to.0}/obj4_naive/")}
   if (iNaive==2) {dat.ph1 = dat.nnaive; save.results.to = glue("{save.results.to.0}/obj4_nnaive/")}
   
+  dat.ph2 = subset(dat.ph1, ph2)
+
   if (!dir.exists(save.results.to))  dir.create(save.results.to)
   print(paste0("save results to ", save.results.to))
   
@@ -268,10 +274,12 @@ for (iNaive in 1:2) {
   # the origin of followup days, may be different from tpeak, e.g., D43start48
   tpeak1 = 29
   
+  
   # fit the interaction model and save regression results to a table
-  for (a in obj.assays) {
-  for (ind in 1:2) { # once for BD29 and one for Delta
-    f= update(form.0, as.formula(paste0("~.+scale(BD1", a ,") * scale(", ifelse(ind==1, "BD29", "DeltaBD29overBD1"), a, ")")))
+  for (ab in config$interaction) {
+    tmp=trim(strsplit(ab, " *\\* *")[[1]]); a=tmp[1]; b=tmp[2]            
+    f= update(form.0, as.formula(paste0("~.+scale(", a ,") * scale(", b, ")")))
+    
     fits=list(svycoxph(f, design=design.1))
     est=getFormattedSummary(fits, exp=T, robust=T, type=1)
     ci= getFormattedSummary(fits, exp=T, robust=T, type=13)
@@ -286,9 +294,8 @@ for (iNaive in 1:2) {
     colnames(tab)=c("HR", "P value")
     # tab=rbind(tab, "Generalized Wald Test for Markers"=c("", formatDouble(p.gwald,3, remove.leading0 = F)))
     tab
-    mytex(tab, file.name=paste0("CoR_itxn_BD1_",ifelse(ind==1, "BD29", "DeltaBD29overBD1"),"_",a), align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
+    mytex(tab, file.name=paste0("CoR_itxn_",a,"_",b), align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
     # itxn.pvals=c(itxn.pvals, last(getFixedEf(fit)[,"p.value"]))
-  }
   }
   
   # names(itxn.pvals)=config$interaction
@@ -298,7 +305,157 @@ for (iNaive in 1:2) {
   # tab=cbind(itxn.pvals, itx.pvals.adj.hol, itx.pvals.adj.fdr)
   # colnames(tab)=c("interaction P value", "FWER", "FDR")
   # mytex(tab, file.name="CoR_itxn_multitesting", align="c", include.colnames = T, save2input.only=T, input.foldername=save.results.to)
-}
+
+  
+  # bootstrap risk curves
+  if(!file.exists(paste0(save.results.to, "itxn.marginalized.risk.Rdata"))) {    
+    cat("make itxn.marginalized.risk\n")
+    
+    risks.itxn=list()      
+    for (ab in config$interaction) {
+      myprint(ab)
+      tmp=trim(strsplit(ab, " *\\* *")[[1]]); a=tmp[1]; b=tmp[2]            
+      f= update(form.0, as.formula(paste0("~.+scale(", a ,") * scale(", b, ")")))
+      
+      fit=svycoxph(f, design=design.1)
+
+      # inner.id 1: treat a as the x axis variable, 2: treat b as the x axis variable
+      # only do 2 because 2 is the BD29 or fold change variables
+      for (inner.id in 2:2) {
+        if (inner.id == 1) {vx=a; vthree=b} else {vx=b; vthree=a}        
+        
+        # compute risks at three values of vthree
+        three.val=wtd.quantile(dat.ph1[[vthree]], dat.ph1$wt, c(.15, .5, .85))                    
+        # compute risks at a sequence of vx values for each of the three vthree values
+        ss=sort(c(
+          wtd.quantile(dat.ph1[[vx]], dat.ph1$wt, c(0.025,0.05,0.95,0.975)), # will be included in the table
+          seq(min(dat.ph1[[vx]], na.rm=TRUE), max(dat.ph1[[vx]], na.rm=TRUE), length=100) # equally spaced between min and max so that the curves look good
+        ))    
+        
+        # estimate marginalized risks, return a matrix
+        prob.ls=sapply (three.val, function(val) {
+          marginalized.risk.cont.2(fit, marker.name  =vx, data=dat.ph2, weights=dat.ph2$wt, t=tfinal.tpeak, ss=ss, 
+                                   marker.name.2=vthree, s.2=val)
+        })
+        
+        #### bootstrap
+        # store the current rng state
+        save.seed <- try(get(".Random.seed", .GlobalEnv), silent=TRUE) 
+        if (class(save.seed)=="try-error") {set.seed(1); save.seed <- get(".Random.seed", .GlobalEnv) }         
+        
+        seeds=1:B; names(seeds)=seeds
+        out=mclapply(seeds, mc.cores = numCores, FUN=function(seed) {   
+          seed=seed+560
+          if (verbose>=2) myprint(seed)
+          
+          dat.b = bootstrap.cove.boost.2(dat.ph1, seed)
+          dat.b.ph2=subset(dat.b, ph2==1)     
+          with(dat.b, table(Wstratum, ph2))     
+          
+          # inline design object b/c it may also throw an error
+          fit.b=try(svycoxph(f, design=twophase(id=list(~1,~1), strata=list(NULL,~Wstratum), subset=~ph2, data=dat.b)))
+          
+          if (!inherits (fit.b, "try-error")) {
+            probs=sapply (three.val, function(val) {
+              marginalized.risk.cont.2(fit.b, marker.name  =vx, data=dat.b.ph2, weights=dat.b.ph2$wt, t=tfinal.tpeak, ss=ss, 
+                                       marker.name.2=vthree, s.2=val)
+            })
+          } else {
+            matrix(NA, length(ss), length(three.val))
+          }
+        })
+        
+        # restore rng state 
+        assign(".Random.seed", save.seed, .GlobalEnv)    
+        
+        # organize bootstrap results into a list of 3, each element of which is a matrix of n.s by n.seeds
+        res.ls=lapply (1:length(three.val), function(i) {
+          res=sapply(out, function (x) x[,i])
+          res[,!is.na(res[1,])] # remove NA's
+        })
+        if (verbose) str(res.ls)
+        # put lb and ub into matrices
+        lb.ls=sapply(res.ls, function (res) t(apply(res, 1, function(x) quantile(x, c(.025)))) )
+        ub.ls=sapply(res.ls, function (res) t(apply(res, 1, function(x) quantile(x, c(.975)))) )
+        
+        risks.itxn[[paste0(vx,vthree)]]=list(marker=ss, prob=prob.ls, boot=res.ls, lb=lb.ls, ub=ub.ls, marker.2=three.val)
+      } # end inner.id
+    }
+    
+    save(risks.itxn, file=paste0(save.results.to, "itxn.marginalized.risk.Rdata"))
+    
+  } else {
+    load(paste0(save.results.to, "itxn.marginalized.risk.Rdata"))
+  }  
+
+  
+  ####
+  cat("make plot interaction risk curves\n")
+  
+  for (ab in config$interaction) {
+    tmp=trim(strsplit(ab, " *\\* *")[[1]]); a=tmp[1]; b=tmp[2]            
+    
+    for (inner.id in 2:2) {
+      if (inner.id == 1) {vx=a; vthree=b} else {vx=b; vthree=a}        
+      vx.assayname = sub("BD[[0123456789]+", "", sub("DeltaBD[[0123456789]+overBD1", "", vx))
+      vthree.assayname = sub("BD[[0123456789]+", "", sub("DeltaBD[[0123456789]+overBD1", "", vthree))
+      
+      risks=risks.itxn[[paste0(vx,vthree)]]
+      
+      mypdf(oma=c(0,0,0,0), onefile=F, file=paste0(save.results.to, "itxn_marginalized_risks_",ifelse(inner.id==1,a,b),"_",ifelse(inner.id==1,b,a)), mfrow=.mfrow)
+      
+      par(las=1, cex.axis=0.9, cex.lab=1)# axis label orientation
+      lwd=2
+      
+      shown=risks$marker>=wtd.quantile(dat.ph1[[vx]], dat.ph1$wt, 2.5/100) & 
+            risks$marker<=wtd.quantile(dat.ph1[[vx]], dat.ph1$wt, 1-2.5/100)
+      
+      # hard code ylim if needed to make the plot look better
+      ylim=range(risks$prob[shown,], 0) # [shown] so that there is not too much empty space
+      xlim=range(risks$marker[shown]) 
+      if(verbose) myprint(xlim, ylim)
+      
+      # set up an empty plot
+      plot(risks$marker[shown], risks$prob[shown,1], 
+           xlab=paste0("BD29/BD1", assay_labels_short[vx.assayname], " (=s)"), 
+           ylab=paste0("Probability* of ",config$txt.endpoint," by Day ", tfinal.tpeak), 
+           lwd=lwd, xlim=xlim, ylim=ylim, type="n", main="", xaxt="n")    
+      draw.x.axis.cor(xlim, NA, NA) # no lloq for fold change
+      
+      # draw risk lines and confidence bands
+      
+      for (i in 1:length(risks$marker.2)) {
+        lines(risks$marker[shown], risks$prob[shown,i], lwd=lwd, col=i, lty=1)# use dashed line for the middle so that overlaps can be seen
+        col <- c(col2rgb(c("black","red","green")[i]))
+        col <- rgb(col[1], col[2], col[3], alpha=255*0.2, maxColorValue=255)
+        lines(risks$marker[shown], risks$lb[shown,i],   lwd=lwd, col=col, lty=1)
+        lines(risks$marker[shown], risks$ub[shown,i],   lwd=lwd, col=col, lty=1)    
+      }
+      
+      # legend for the three lines
+      legend.txt=c("(15th percentile)","(median)","(85th percentile)")
+      mylegend(x=3, legend=paste(signif(10**risks$marker.2, 3), legend.txt), col=1:3, lty=c(1,2,1), title=paste0("BD1 ", assay_labels_short[vthree.assayname]), lwd=lwd)
+      
+      # # placebo prevalance lines
+      # abline(h=prev.plac[1], col="gray", lty=c(1,3,3), lwd=lwd)
+      # text(x=par("usr")[2]-diff(par("usr")[1:2])/5, y=prev.plac[1]+diff(par("usr")[3:4])/30, "placebo arm "%.%formatDouble(prev.plac[1],3,remove.leading0=F))        
+
+      # add histogram
+      par(new=TRUE) 
+      col <- c(col2rgb("olivedrab3")) # orange, darkgoldenrod2
+      col <- rgb(col[1], col[2], col[3], alpha=255*0.4, maxColorValue=255)
+      tmp.x=dat.ph1[[vx]][dat.ph1$ph2]
+      tmp.w=dat.ph1$wt[dat.ph1$ph2]
+      tmp=get.marker.histogram(tmp.x, tmp.w, attr(config,"config"))
+      if (is.nan(tmp$density)[1]) tmp=hist(tmp.x, plot=F)
+      plot(tmp,col=col,axes=F,labels=F,main="",xlab="",ylab="",border=0,freq=F, xlim=xlim, ylim=c(0,max(tmp$density*1.25)))
+      
+      dev.off()            
+    }
+  }  
+  
+  
+} # end if iNaive
 
 
 # # joint distribution of BD1 and BD29 markers
@@ -311,8 +468,7 @@ for (iNaive in 1:2) {
 
 
 ###################################################################################################
-# multivariate_assays models
-# pooled over naive and nnaive
+# multivariate_assays models in naive + nnaive
 
 if (!is.null(config$multivariate_assays)) {
   if(verbose) print("Multiple regression")
